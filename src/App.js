2bbc921cc42c4eb15056bb953af00bfe78c70f8c
Arnav Tiwari <106868854+ArnavO3@users.noreply.github.com>
2022-11-10 19:30:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Header from "./components/header/header";
+import Home from "./components/home/home";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+function Home(){
+    return(
+        <>
+        <div className="home">HOME PAGE</div>
+        </>
+    );
+}
+
+export default Home();
